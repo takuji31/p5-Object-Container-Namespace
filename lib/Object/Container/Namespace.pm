48d4c7cadf2e112785e7f1b0dc9855a19e7da8bd
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-16 08:43:34 +0900

    とりあえず形だけ…。

--- lib/Object/Container/Namespace.pm
+++ lib/Object/Container/Namespace.pm
@@ -1,7 +1,82 @@
 package Object::Container::Namespace;
 use strict;
 use warnings;
+
+use String::CamelCase qw/camelize/;
+use Carp();
+use base qw/Class::Singleton/;
+
 our $VERSION = '0.01';
+
+sub import {
+    my $class  = shift;
+    my $caller = caller;
+
+    if( $_[0] && $_[0] eq '-base' ) {
+        # base
+
+        {
+            no strict 'refs';
+            push @{"$caller\::ISA"},$class;
+        }
+
+        my $instance_table = {};
+        my $registered_class = {};
+        {
+            no strict 'refs';
+            *{"$caller\::_instance_table"} = sub { $instance_table };
+            *{"$caller\::_registered_classes"} = sub { $registered_class };
+        }
+
+    }
+    else {
+        # export methods
+
+    }
+
+    $caller->initialize(@_);
+}
+
+sub initialize { 'DUMMY' }
+
+sub register {
+    my $class       = shift;
+    my $pkg         = shift;
+    my $initializer = $_[0];
+    my @options     = @_;
+
+    unless ( $pkg ) {
+        Carp::croak("Register name is empty!");
+    }
+    unless ( defined $initializer && ref($initializer) eq 'CODE' && scalar @options == 1 ){
+        $initializer = sub {
+            my $self = shift;
+            $self->load_class($pkg);
+            $pkg->new(@options);
+        }
+    }
+
+    #register classes
+    $class->_registered_classes->{$pkg} = $initializer;
+}
+
+sub get {
+    my ( $class, $pkg ) = @_;
+
+    my $self = ref($class) ? $class : $class->instance;
+
+    my $obj = $class->_instance_table->{$pkg};
+
+    unless ( $obj ) {
+        my $code = $class->_registered_class->{$pkg};
+        unless ( $code ) {
+            Carp::croak("$pkg is not registered!");
+        }
+        $obj = $code->($self);
+    }
+
+    return $obj;
+}
 
 1;
 __END__
