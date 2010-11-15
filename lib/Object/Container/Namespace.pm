6d7b4522cbf345b9a4dbc39689d39ec0a76b4854
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-16 01:21:06 +0900

    initial commit

---
+++ lib/Object/Container/Namespace.pm
@@ -0,0 +1,32 @@
+package Object::Container::Namespace;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Object::Container::Namespace -
+
+=head1 SYNOPSIS
+
+  use Object::Container::Namespace;
+
+=head1 DESCRIPTION
+
+Object::Container::Namespace is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
